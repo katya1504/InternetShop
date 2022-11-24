d70b07d67ec2a1fb559a6dab3a69491acb289960
katya1504 <katya150491@gmail.ru>
2022-11-24 20:54:30 +0300

    your message

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import Contact from './Component/furnitureComponent/Contact';
 function App() {
   return <Router>
     <nav className='menu'>
-     <Link to='/NavBar' className='link'>Главная</Link> 
+     <Link to='/' className='link'>Главная</Link> 
      <Link to='/Catalog' className='link'>Каталог</Link> 
      <Link to='/Blog' className='link'>Фото</Link>
      <Link to='/Cart' className='link'><img src='https://cdn-icons-png.flaticon.com/512/1011/1011286.png?w=740&t=st=1668763195~exp=1668763795~hmac=646c869aa9872be610dbd8ababedaf40a8779b451a2d87cc0eb9b835877d2144' width='25px' height='20px'  alt='Urn'></img></Link>
@@ -29,7 +29,7 @@ function App() {
     </div>
 
     <Routes>
-    <Route path='/NavBar' element={<NavBar/>} />
+    <Route path='/' element={<NavBar/>} />
       <Route path='/Catalog' element={<Catalog/>} />
     <Route path='/Blog' element={<Blog/>} />
     <Route path='/Cart' element={<Cart/>} />
